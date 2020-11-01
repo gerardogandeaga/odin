c5e17e7fff3efea8966c23b2756279c02ab2bb57
Gerardo Gandeaga <gerardogandeaga@gmail.com>
2020-10-31 17:28:52 -0700

    Changed Firebase reference to Odin admin account.

--- app/src/main/java/com/group8/odin/MainActivity.java
+++ app/src/main/java/com/group8/odin/MainActivity.java
@@ -1,11 +1,28 @@
 package com.group8.odin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.UUID;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /*
  * Created by: Gerardo Gandeaga
@@ -13,12 +30,87 @@ import butterknife.BindView;
  * Description:
  */
 public class MainActivity extends AppCompatActivity {
-    @BindView(R.id.tvHeaderTitle)
-    TextView mHeader;
+    @BindView(R.id.imgAuthResult)
+    ImageView mImgResult;
+    @BindView(R.id.btnStartCamera)
+    Button mBtnStartCamera;
+    @BindView(R.id.btnUpload)
+    Button mBtnUpload;
+
+    // Define the pic id
+    private static final int PIC_ID = 123;
+
+    // Reference
+    private Uri mImagePath;
+
+    // Firebase
+    FirebaseStorage firebaseStorage;
+    StorageReference storageReference;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        // Connect to firebase
+        firebaseStorage = FirebaseStorage.getInstance();
+        storageReference = firebaseStorage.getReference();
+
+        // Camera_open button is for open the camera
+        // and add the setOnClickListener in this button
+        mBtnStartCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                // Create the camera_intent ACTION_IMAGE_CAPTURE
+                // it will open the camera for capture the image
+                Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+                // Start the activity with camera_intent,
+                // and request pic id
+                startActivityForResult(cameraIntent, PIC_ID);
+            }
+        });
+
+        mBtnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Upload image
+                StorageReference ref = storageReference.child("CMPT-276-E100/");
+                ref.putFile(mImagePath).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        Toast.makeText(MainActivity.this, "Image uploaded!", Toast.LENGTH_LONG).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this, "Error!", Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        });
+    }
+
+    // This method will help to retrieve the image
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        // Match the request 'pic id with requestCode
+        if (requestCode == PIC_ID) {
+
+            // BitMap is data structure of image file
+            // which store the image in memory
+            Bitmap photo = (Bitmap) data.getExtras()
+                    .get("data");
+
+            // Set the image in imageview for display
+            mImgResult.setImageBitmap(photo);
+
+            mImagePath = data.getData();
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
     }
 }
---
+++ app/src/main/java/com/group8/odin/Press.java
@@ -0,0 +1,15 @@
+package com.group8.odin;
+
+import android.view.View;
+
+/*
+ * Created by: Gerardo Gandeaga
+ * Created on: 2020-10-31
+ * Description:
+ */
+public class Press implements View.OnClickListener {
+    @Override
+    public void onClick(View view) {
+        // logic
+    }
+}
